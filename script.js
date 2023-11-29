f680a60e2a96e272830e9f2f0e1e64847ce8919f
SruthiDamera23 <147087974+SruthiDamera23@users.noreply.github.com>
2023-11-28 22:47:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function openModal() {
+    document.getElementById('uploadModal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('uploadModal').style.display = 'none';
+}
+
+function uploadRecipe() {
+    // Implement your recipe upload logic here
+    // For now, just close the modal
+    closeModal();
+}
