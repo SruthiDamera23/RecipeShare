41748fef8cac7d2fa196ddb9756ed638d786e6b5
SruthiDamera23 <147087974+SruthiDamera23@users.noreply.github.com>
2023-12-06 04:25:21 +0530

    Add files via upload

--- upload-recipe.js
+++ upload-recipe.js
@@ -5,7 +5,9 @@ function openModal() {
 }
 
 function closeModal() {
+
     document.getElementById('uploadModal').style.display = 'none';
+    window.location.href = 'my-recipes.html';
 }
 
 
@@ -42,7 +44,7 @@ function uploadRecipe() {
     // Create a new recipe object
     var newRecipe = {
         "recipe_id": recipe_id,
-        "image_url": "https://example.com/recipe1.jpg",
+        "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4PYEJAG6SDy3u5i1SIdcj496OZn_Itb6cs-BRPRGmkwBYbGviDEL2lPZ8yWbl7yYTRwI&usqp=CAU",
         "title": recipeTitle,
         "instructions": cookingInstructions,
         "ingredients": ingredients,
@@ -58,12 +60,30 @@ function uploadRecipe() {
     // // Add the new recipe to the array
     recipes.push(newRecipe);
 
+    
 
     // Save the updated recipes back to local storage
     localStorage.setItem('recipes', JSON.stringify(recipes)); 
     var existingRecipes = localStorage.getItem('recipes');
+    window.location.href = 'my-recipes.html';
 
-    // Close the modal
-    closeModal();
+    showUploadSuccessMessage();
+    // // Close the modal
+    // closeModal();
+}
+
+function showUploadSuccessMessage() {
+    var successMessage = document.getElementById('uploadSuccessMessage');
+    console.log("xyz")
+    // Set the content of the message
+    successMessage.textContent = 'Upload successful!';
+
+    // Display the message with the 'show' class
+    successMessage.classList.add('show');
+
+    // Hide the message after a certain duration (e.g., 3 seconds)
+    setTimeout(function () {
+        successMessage.classList.remove('show');
+    }, 3000); // 3000 milliseconds = 3 seconds
 }
 
